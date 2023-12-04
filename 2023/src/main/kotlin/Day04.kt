e4d24d10eb6ac69803084a8da2558df675a1ad4f
Kevin Cheung <kevin@owmyneck.com>
2023-12-04 14:50:13 -0800

    Day 4, part 2

--- 2023/src/main/kotlin/Day04.kt
+++ 2023/src/main/kotlin/Day04.kt
@@ -1,6 +1,8 @@
 import kotlin.math.pow
 
 class Day04 : AdventSolution() {
+    data class ScratchCard(val id: Int, val winners: Set<Int>, val picks: Set<Int>)
+
     override fun solvePart1(input: List<String>): String {
         val cardPoints = input.map { card ->
             val parsedCard = cardRegex.matchEntire(card) ?: throw Error("Bad card.")
@@ -14,7 +16,25 @@ class Day04 : AdventSolution() {
     }
 
     override fun solvePart2(input: List<String>): String {
-        TODO("Not yet implemented")
+        val scratcherdex = mutableMapOf<Int, ScratchCard>()
+        for (card in input) {
+            val parsedCard = cardRegex.matchEntire(card) ?: throw Error("Bad card.")
+            val id = parsedCard.groupValues[1].toInt()
+            val winners = parsedCard.groupValues[2].trim().split(Regex("\\s+")).map { it.toInt() }.toSet()
+            val picks = parsedCard.groupValues[3].trim().split(Regex("\\s+")).map { it.toInt() }.toSet()
+            scratcherdex[id] = ScratchCard(id, winners, picks)
+        }
+        val myCards = scratcherdex.values.toMutableList()
+        var i = 0
+        while (i < myCards.size) {
+            val card = myCards[i]
+            val numMatches = (card.winners intersect card.picks).size
+            for (n in 1..numMatches) {
+                myCards.addLast(scratcherdex[card.id + n])
+            }
+            i += 1
+        }
+        return myCards.size.toString()
     }
 
     companion object {
--- 2023/src/test/kotlin/Day04Test.kt
+++ 2023/src/test/kotlin/Day04Test.kt
@@ -1,6 +1,5 @@
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
 
 class Day04Test {
     private val solver = Day04()
@@ -16,5 +15,18 @@ class Day04Test {
             Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
         """.trimIndent().lines()
         assertEquals("13.0", solver.solvePart1(input))
+    }
+
+    @Test
+    fun solvePart2() {
+        val input = """
+            Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+            Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+            Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+            Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+            Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+            Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+        """.trimIndent().lines()
+        assertEquals("30", solver.solvePart2(input))
     }
 }
