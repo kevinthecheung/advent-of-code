fdd405b2e5d358126fc365852266fce44ce3ee24
Kevin Cheung <kevin@owmyneck.com>
2023-12-07 10:19:58 -0800

    Day 7, part 2

--- 2023/src/main/kotlin/Day07.kt
+++ 2023/src/main/kotlin/Day07.kt
@@ -10,7 +10,13 @@ class Day07 : AdventSolution() {
     }
 
     override fun solvePart2(input: List<String>): String {
-        TODO("Not yet implemented")
+        val handsAndBids = input.map {
+            val (h, b) = it.split(Regex("\\s+"))
+            Pair(h, b.toInt())
+        }.sortedWith { a, b -> handComparatorJoker.compare(a.first, b.first) }
+        val winnings = handsAndBids.mapIndexed { i, hb -> hb.second * (i + 1) }
+        val total = winnings.sum()
+        return total.toString()
     }
 
     companion object {
@@ -38,6 +44,37 @@ class Day07 : AdventSolution() {
             for (i in a.indices) {
                 if (a[i] != b[i]) {
                     return@Comparator cardLabels.indexOf(a[i]) - cardLabels.indexOf(b[i])
+                }
+            }
+            0
+        }
+
+        val cardLabelsJoker = listOf('J', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'T', '?', 'Q', 'K', 'A')
+
+        fun handValueJoker(hand: String): Int {
+            val labelCounts = cardLabelsJoker.map { c -> hand.filter { it == c }.length }
+            val nonJokerCounts = labelCounts.slice(1..<labelCounts.size).sortedDescending()
+            val jokerCount = labelCounts[0]
+            return when {
+                (nonJokerCounts.max() + jokerCount) == 5 -> 6
+                (nonJokerCounts.max() + jokerCount) == 4 -> 5
+                (nonJokerCounts[0] + nonJokerCounts[1] + jokerCount) == 5 -> 4
+                (nonJokerCounts.max() + jokerCount) == 3 -> 3
+                (nonJokerCounts[0] + nonJokerCounts[1] + jokerCount) == 4 -> 2
+                (nonJokerCounts[0] + jokerCount) == 2 -> 1
+                else -> 0
+            }
+        }
+
+        val handComparatorJoker = Comparator<String> { a, b ->
+            val aVal = handValueJoker(a)
+            val bVal = handValueJoker(b)
+            if (aVal != bVal) {
+                return@Comparator aVal - bVal
+            }
+            for (i in a.indices) {
+                if (a[i] != b[i]) {
+                    return@Comparator cardLabelsJoker.indexOf(a[i]) - cardLabelsJoker.indexOf(b[i])
                 }
             }
             0
--- 2023/src/test/kotlin/Day07Test.kt
+++ 2023/src/test/kotlin/Day07Test.kt
@@ -16,4 +16,16 @@ class Day07Test {
         """.trimIndent().lines()
         assertEquals("6440", solver.solvePart1(input))
     }
+
+    @Test
+    fun solvePart2() {
+        val input = """
+            32T3K 765
+            T55J5 684
+            KK677 28
+            KTJJT 220
+            QQQJA 483
+        """.trimIndent().lines()
+        assertEquals("5905", solver.solvePart2(input))
+    }
 }
